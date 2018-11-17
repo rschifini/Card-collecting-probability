15932b172cd8a9345f7cff1ee3a265678e8d9e14
Ricardo Schifini <rschifini@gmail.com>
2018-11-17 19:44:12 -0300

    typo in graph

--- One person simulations.R
+++ One person simulations.R
@@ -30,5 +30,6 @@ for(k in 1:simulations){
   totalPacksOpened = c(totalPacksOpened,openedPacks)
 }
 
-hist(totalPacksOpened, breaks = 50, xlim = c(100,600), freq = F, ylim = c(0,.01), main = "Probability density of packs opened by one person")
+hist(totalPacksOpened, breaks = 50, xlim = c(100,600), 
+     freq = F, ylim = c(0,.01), main = "Probability density of packs opened by one person")
 lines(density(totalPacksOpened, bw = 7), ylim = c(0, 0.01), xlim = c(100,600), col = "red")
